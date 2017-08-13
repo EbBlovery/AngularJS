d0879ca1beef6078baae100b0cbb913d1952447b
EbBlovery <18217052735@163.com>
2017-08-13 14:13:32 +0800

    front of route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 // export class Hero {
 //   id: number;
 //   name: string;
 // }
-const HEROES: Hero[] = [
-{ id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
+// const HEROES: Hero[] = [
+// { id: 11, name: 'Mr. Nice' },
+//   { id: 12, name: 'Narco' },
+//   { id: 13, name: 'Bombasto' },
+//   { id: 14, name: 'Celeritas' },
+//   { id: 15, name: 'Magneta' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 17, name: 'Dynama' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' }
+// ];
 @Component({
   selector: 'app-root',
   template: `
@@ -77,18 +79,26 @@ const HEROES: Hero[] = [
         	margin-right: .8em;
         	border-radius: 4px 0 0 4px;
         }
-    `]
+    `],
+    providers: [HeroService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private heroService: HeroService){}
   title = 'Tour of Heroes';
   // hero: Hero = {
   //   id: 1,
   //   name: 'Windstorm'
   // };
-  selectedHero:Hero;
-  heroes = HEROES;
+  heroes: Hero[];
+  selectedHero:Hero; //声明selectedHero 为Hero类型；
+  // heroes = HEROES;
   onSelect(hero: Hero): void {
   	this.selectedHero = hero;
-  	console.log(this.selectedHero)
+  }
+  getHeroes(): void{
+  	this.heroService.getHeroes().then(res =>this.heroes = res);
+  }
+  ngOnInit(): void {
+  	this.getHeroes();
   }
 }
